b617d5531d072caded3e66c431a8f08549c8bc00
Krypton Yousuke <kryptonyousuke@gmail.com>
2026-08-30 15:31:49 -0300

    [build] Refactor build.rs
    Signed-off-by: Krypton Yousuke <kryptonyousuke@gmail.com>

--- build.rs
+++ build.rs
@@ -1,14 +1,11 @@
 // build.rs
 use shaderc::{Compiler, ShaderKind};
-use std::fs;
 use std::path::Path;
 
-fn main() {
-    println!("cargo:rerun-if-changed=shaders/");
-    let compiler = Compiler::new().unwrap();
-    let shader_dir = Path::new("shaders");
+/// Build all the needed shaders.
+fn build_shaders(compiler: &Compiler, shader_dir: &Path, out_dir: &String) {
     if shader_dir.is_dir() {
-        for entry in fs::read_dir(shader_dir).unwrap() {
+        for entry in std::fs::read_dir(shader_dir).unwrap() {
             let entry = entry.unwrap();
             let path = entry.path();
 
@@ -20,17 +17,25 @@ fn main() {
                     _ => continue,
                 };
 
-                let shader_source = fs::read_to_string(&path).unwrap();
+                let shader_source = std::fs::read_to_string(&path).unwrap();
                 let file_name = path.file_name().unwrap().to_str().unwrap();
 
                 let artifact = compiler
                     .compile_into_spirv(&shader_source, kind, file_name, "main", None)
                     .unwrap();
 
-                let out_dir = std::env::var("OUT_DIR").unwrap();
                 let output_path = Path::new(&out_dir).join(format!("{file_name}.spv"));
-                fs::write(output_path, artifact.as_binary_u8()).unwrap();
+                std::fs::write(output_path, artifact.as_binary_u8()).unwrap();
             }
         }
     }
+}
+
+fn main() {
+    println!("cargo:rerun-if-changed=shaders/");
+    let compiler = Compiler::new().unwrap();
+    let shader_dir = Path::new("shaders");
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+
+    build_shaders(&compiler, shader_dir, &out_dir);
 }
